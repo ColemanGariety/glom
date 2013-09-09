8fc3730fbc134a5ca5324ac77ed2b0eff86508c7
Ben Martin <tennisracket22@yahoo.com>
2013-09-08 18:51:52 -0700

    set up Glom class

--- lib/glom.rb
+++ lib/glom.rb
@@ -1,5 +1,26 @@
 require "glom/version"
+require "net/http"
 
-module Glom
-  # Your code goes here...
+class Glom
+  
+  # 1. Curl from bower-component-list.herokuapp.com, npmjs.org/package/[package], and rubygems.org/gems/[name]
+  # 2. Parse JSON from bower to and direct to Github page for keyword matching
+  # 3. 
+  # glom [term] => 
+
+ 
+# uri = URI('http://example.com/index.html')
+# params = { :limit => 10, :page => 3 }
+# uri.query = URI.encode_www_form(params)
+# 
+# res = Net::HTTP.get_response(uri)
+# puts res.body if res.is_a?(Net::HTTPSuccess)
+  
+	def initialize
+		
+	end  
 end
+
+glom = Glom.new
+
+
--- lib/glom/version.rb
+++ lib/glom/version.rb
@@ -1,3 +1,3 @@
-module Glom
+class Glom
   VERSION = "0.0.1"
 end
